ca415331ca7f77bc93e5c4a9d8a86258dad51f63
Xavi <email@javier-rodriguez.com>
2018-01-14 17:39:54 +0100

    First Commit

---
+++ PROYECTO1/js/GamePlay.js
@@ -0,0 +1,22 @@
+GamePlayManager =
+{
+  init: function(){
+    console.log('init');
+  },
+  preload: function(){
+    console.log('preload');
+  },
+  create: function(){
+    console.log('create');
+  },
+  update: function(){
+    console.log('update');
+  }
+}
+
+
+
+var game = new Phaser.Game(1136, 640, Phaser.auto);
+
+game.state.add('gameplay', GamePlayManager);
+game.state.start('gameplay');
